989dea2dea5254ff02c41ad5197c2102af78c56f
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-17 11:00:10 +0530

    Added : Program For Create A Contact

--- src/com/bl/day22/AddressBookMain.java
+++ src/com/bl/day22/AddressBookMain.java
@@ -3,5 +3,16 @@ package com.bl.day22;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program...!!!");
-    }
+       Contact newContact = new Contact();
+        newContact.setFirstName("Sandipan\n");
+        newContact.setLastName("Kanade\n");
+        newContact.setAddressCity("Shirdi\n");
+        newContact.setState("Maharashtra\n");
+        newContact.setEmail("sandipank@gmail.com");
+        newContact.setZip(414501);
+        newContact.setPhoneNumber( 9822726196L);
+        System.out.println("The Contact is :\n" +newContact);
+
 }
+}
+
--- src/com/bl/day22/Contact.java
+++ src/com/bl/day22/Contact.java
@@ -1,4 +1,82 @@
 package com.bl.day22;
 
 public class Contact {
+    private String firstName;
+    private String lastName;
+    private String addressCity;
+    private String state;
+    private int zip;
+    private long phoneNumber;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName
+            (String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddressCity() {
+        return addressCity;
+    }
+
+    public void setAddressCity(String addressCity) {
+        this.addressCity = addressCity;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getPhoneNumber() {
+        return (int) phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "firstName= " + firstName + '\'' +
+                        "lastName=" + lastName + '\'' +
+                        "addressCity=" + addressCity + '\'' +
+                        "state=" + state + '\'' +
+                        "email=" + email + '\''+
+                        "zip=" + zip +'\''+
+                        "phoneNumber=" + phoneNumber + '\'';
+
+    }
 }
+
