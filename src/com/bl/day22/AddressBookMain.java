fe9fff39be68936465470009b8b6cf71008ab2e6
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-17 11:10:05 +0530

    Added : Program for Add New Contact to Address Book Using Console

--- src/com/bl/day22/AddressBookMain.java
+++ src/com/bl/day22/AddressBookMain.java
@@ -3,15 +3,8 @@ package com.bl.day22;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program...!!!");
-       Contact newContact = new Contact();
-        newContact.setFirstName("Sandipan\n");
-        newContact.setLastName("Kanade\n");
-        newContact.setAddressCity("Shirdi\n");
-        newContact.setState("Maharashtra\n");
-        newContact.setEmail("sandipank@gmail.com");
-        newContact.setZip(414501);
-        newContact.setPhoneNumber( 9822726196L);
-        System.out.println("The Contact is :\n" +newContact);
+        NewAddressBook person = new NewAddressBook();
+        person.addContact();
 
 }
 }
---
+++ src/com/bl/day22/NewAddressBook.java
@@ -0,0 +1,35 @@
+package com.bl.day22;
+
+import java.util.Scanner;
+
+public class NewAddressBook {
+    Scanner sc = new Scanner(System.in);
+
+    public void addContact() {
+        Contact person = new Contact();
+        System.out.println("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter last Name: ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter your addressCity: ");
+        String addressCity = sc.nextLine();
+        System.out.println("Enter your state: ");
+        String state = sc.nextLine();
+        System.out.println("Enter zip code : ");
+        Long zip = sc.nextLong();
+        System.out.println("Enter phone number: ");
+        Long phoneNumber = sc.nextLong();
+        System.out.println("Enter your EMail ID: ");
+        String email = sc.nextLine();
+
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        person.setAddressCity(addressCity);
+        person.setState(state);
+        person.setZip(zip.intValue());
+        person.setPhoneNumber(phoneNumber.intValue());
+        person.setEmail(email);
+        System.out.println("The Contact Details of " + firstName + " " + lastName + "\n" + person);
+    }
+}
+
